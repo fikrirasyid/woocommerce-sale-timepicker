929df8ecf1ef0198bc249e1a817204155a2d951e
Fikri Rasyid <fikrirasyid@gmail.com>
2014-07-15 17:28:05 +0700

    Adding appropriate comments

--- js/woocommerce-sale-timepicker.js
+++ js/woocommerce-sale-timepicker.js
@@ -6,10 +6,6 @@ jQuery(document).ready(function($) {
 	// Destroy existing datepicker init
 	$( ".sale_price_dates_fields input" ).datetimepicker( 'destroy' );
 
-	// Update the pattern attribute to accept datetime
-	// $( ".sale_price_dates_fields input" ).attr({ 'pattern' : '[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01]) (0[1-9]|1[023]):(0[1-9]|1[059])' });
-	// $( ".sale_price_dates_fields input" ).removeAttr( 'pattern' );
-
 	// Rebuilding the UX, using datetimepicker
 	var dates = $( ".sale_price_dates_fields input" ).datetimepicker({
 		defaultDate: "",
@@ -22,17 +18,7 @@ jQuery(document).ready(function($) {
 		buttonImageOnly: true,
 		onSelect: function( selectedDate ) {
 			var option = $(this).is('#_sale_price_dates_from, .sale_price_dates_from') ? "minDate" : "maxDate";
-
-			// var instance = $( this ).data( "datepicker" ),
-			// 	date = $.datepicker.parseDate(
-			// 		instance.settings.dateFormat ||
-			// 		$.datepicker._defaults.dateFormat,
-			// 		selectedDate, instance.settings );
 			var date = $(this).datepicker('getDate');
-
-			console.log( selectedDate );
-	
-			console.log( date );
 			dates.not( this ).datetimepicker( "option", option, date );
 		}
 	});
--- woocommerce-sale-timepicker.php
+++ woocommerce-sale-timepicker.php
@@ -24,47 +24,57 @@ class Woocommerce_Sale_Timepicker{
 		$this->plugin_url = untrailingslashit( plugins_url( '/', __FILE__ ) );
 		$this->plugin_dir = dirname( __FILE__ );
 
+		// Adding external files
 		$this->requiring();
 
+		// Enqueueing scripts
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
 
+		// Removing and adding meta box
 		add_action( 'add_meta_boxes', array( $this, 'remove_meta_boxes' ), 40 );
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ), 50 );
 	}
 
 	/**
-	 * Requiring file(s)
+	 * Requiring external files file(s)
 	 */
 	function requiring(){
 		require_once( $this->plugin_dir . '/includes/wc-sl-meta-box-product-data.php' );
 	}
 
 	/**
-	 * Register script
+	 * Register and enqueue script
 	 */
 	function admin_scripts(){
 		wp_register_script( 'jquery-ui-timepicker', $this->plugin_url . '/js/jquery-ui-timepicker.js', array( 'jquery', 'jquery-ui-datepicker' ) );
 		wp_register_script( 'woocommerce_admin_sale_timepicker', $this->plugin_url . '/js/woocommerce-sale-timepicker.js', array( 'jquery', 'jquery-ui-datepicker', 'jquery-ui-sortable', 'accounting', 'round', 'jquery-ui-timepicker', 'woocommerce_admin_meta_boxes' ) );
 
+		// Get current screen estate
 		$screen = get_current_screen();
 
+		// Only enqueue the script on add new / edit product dashboard
 		if( 'product' == $screen->id ){
 	    	wp_enqueue_script( 'woocommerce_admin_sale_timepicker' );
 		}
 	}
 
 	/**
-	 * Remove meta box
+	 * Removing product data meta box which comes with WooCommerce
+	 * 
+	 * @return void
 	 */
 	public function remove_meta_boxes() {
 		remove_meta_box( 'woocommerce-product-data', 'product', 'normal' );
 	}
 
 	/**
-	 * Add WC Meta boxes
+	 * Adding custom meta box that is slightly modified for timepicking purpose
+	 * It is basically extending the original class with slight modification on the method
+	 * 
+	 * @return void
 	 */
 	function add_meta_boxes(){
-		add_meta_box( 'woocommerce-product-data', __( 'Product Data M', 'woocommerce' ), 'WC_SL_Meta_Box_Product_Data::output', 'product', 'normal', 'high' );		
+		add_meta_box( 'woocommerce-product-data', __( 'Product Data', 'woocommerce' ), 'WC_SL_Meta_Box_Product_Data::output', 'product', 'normal', 'high' );		
 	}	
 }
 new Woocommerce_Sale_Timepicker;
